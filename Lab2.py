d638d63046956424fac9070625a7c5fbc3e81e28
Cao Zheran <zheran.21@ichat.sp.edu.sg>
2022-04-29 16:53:41 +0800

    Initial version to display text message

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,47 @@
-print("ET0735 (DevOps for AIoT) - Lab2 - Introduction to Python")
+def main():
+    print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+    print(calc_average_temperature(num_list))
+    print(calc_min_max_temperature(num_list))
+
+def display_main_menu():
+    print("display_main_menu")
+    print("Enter some numbers separated by commas(e.g.5,67,32)")
+
+def get_user_input():
+    input_num = input()
+    list_input = input_num.split(",")
+    float_list = [float(number) for number in list_input]
+
+    return float_list
+
+def calc_average_temperature(num_input):
+    total = 0
+    count = 0
+    for number in num_input:
+        count += 1
+
+    for number in num_input:
+        total = total + number
+
+    ave = total / count
+
+    return ave
+
+def calc_min_max_temperature(num_input):
+    smallest = largest = num_input[0]
+
+    for i in num_input:
+        if i > largest:
+            largest = i
+
+    for i in num_input:
+        if i < smallest:
+            smallest = i
+
+    max_min_ = [smallest, largest]
+    return max_min_
+
+if __name__ == "__main__":
+    main()
